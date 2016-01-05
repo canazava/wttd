4d8240e0053ef325f157d15b2532319895df078e
canazava <brunocanazava@gmail.com>
2016-01-05 15:24:35 -0200

    Instala Django-Extensions

--- eventex/settings.py
+++ eventex/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third-Party Apps
+    'django_extensions',
+    #My Apps
     'eventex.core',
     'eventex.subscriptions',
 
