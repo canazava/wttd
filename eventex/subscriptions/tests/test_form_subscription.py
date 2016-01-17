3714dcf1a3b9a2119ff6768d26f71de04330b070
canazava <brunocanazava@gmail.com>
2016-01-17 15:53:13 -0200

    Valida CPF

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -1,14 +1,46 @@
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
-
 class SubscriptionFormClass(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_field(self):
-            """
-            :return: Form must have 4 fields
-            """
-            expected = ['name', 'cpf', 'email', 'phone']
-            self.assertSequenceEqual(expected, list(self.form.fields))
+        """
+        :return: Form must have 4 fields
+        """
+        form = SubscriptionForm()
+        expected = ['name', 'cpf', 'email', 'phone']
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digit(self):
+        """
+        :return: DPF must only accept digits
+        """
+        form = self.make_validated_form(cpf='ABCD5678901')
+        # self.assertFormErrorMessage(form, 'cpf', 'CPF deve conter apenas números!')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        """
+        :return: CPF must have 11 digits
+        """
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def assertFormErrorMessage(self, form, field, msg):
+        errors = form.errors
+        errors_list = errors[field]
+        self.assertListEqual([msg], errors_list)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Bruno Canazava', cpf='12345678901',
+                    email='brunocanazava@gmail.com', phone='12-34567-0120')
+        data = dict(valid, **kwargs)
+
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
